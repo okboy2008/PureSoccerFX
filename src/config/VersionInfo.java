7114fd8016e295a8d155aad10348bfc67a24ac76
okboy <okboy2008@gmail.com>
2016-07-05 00:17:33 +0200

    test new user

--- src/config/VersionInfo.java
+++ src/config/VersionInfo.java
@@ -14,4 +14,5 @@ public class VersionInfo {
      public static final String VERSION = "Beta 1.0";
      public static final String DATE = "2016-06-30";
      public static final String AUTHOR = "Yujie Wang";
+     public static final String EMAIL = "y.wang@student.tue.nl";
 }
